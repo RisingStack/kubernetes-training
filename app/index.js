a119f5cff353d76eabbbb9aa40aa66276dbcba14
Roland Szoke <fwalkwithm@gmail.com>
2018-01-11 14:36:42 +0100

    feat(k8s): gracefull shutdown

--- app/index.js
+++ app/index.js
@@ -1,19 +1,37 @@
 const express = require('express')
 const app = express()
 
-let healthy = true
-
-if(process.env.SHOULD_FAIL_WITHIN) {
-  setTimeout(() => { healthy = false }, process.env.SHOULD_FAIL_WITHIN)
+// READINESS_PROBE_DELAY = failureThreshold * periodSeconds * 1000
+const READINESS_PROBE_DELAY = 4000
+const state = { 
+  isShutdown: false,
+  healthy: true
 }
+console.log(process.pid)
+if(process.env.SHOULD_FAIL_WITHIN) {
+  setTimeout(() => { state.healthy = false }, process.env.SHOULD_FAIL_WITHIN)
+}
+
+process.on('SIGTERM', () => { 
+  state.isShutdown = true
+  setTimeout( function gracefulShutdown () {
+    server.close()
+    // db close
+    process.exit()
+  }, READINESS_PROBE_DELAY)
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/healthz', (req, res) => {
-  if(healthy) {
+  if (state.isShutdown) {
+    return res.send(503, 'Service is shutting down')
+  }
+
+  if(state.healthy) {
     return res.sendStatus(200)
   } else {
     return res.sendStatus(500)
   }
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+const server = app.listen(3000, () => console.log('Example app listening on port 3000!'))
