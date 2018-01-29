cd20b31d7a90cfa7bcced09caedef22988c85882
Roland Szoke <fwalkwithm@gmail.com>
2018-01-29 12:56:52 +0100

    feat(istio): routerule all to v1

--- istio_examples/RouteRule/deploy.sh
+++ istio_examples/RouteRule/deploy.sh
@@ -7,12 +7,12 @@ SERVICE_NAMES=("ui" "api")
 for SERVICE_NAME in "${SERVICE_NAMES[@]}"; do
   cd $DIR/$SERVICE_NAME/src && npm i && npm run dockerize
   kubectl delete service,deployment -l app=$SERVICE_NAME
-  kubectl create -f ../deployment.yml
-  kubectl create -f ../service.yml
+  kubectl apply -f <(istioctl kube-inject -f ../deployment.yml)
+  kubectl apply -f <(istioctl kube-inject -f ../service.yml)
 done
 
 API_URL=$(minikube service api --url)
 kubectl patch deployment ui --patch '{"spec": {"template": {"spec": {"containers": [{"name": "ui", "env":[{"name": "REACT_APP_API_URL", "value": "'$API_URL'"}]}]}}}}'
 
 kubectl delete routerule -l app=api
-kubectl create -f $DIR/api/routerule.yml
+kubectl apply -f <(istioctl kube-inject -f $DIR/api/routerule.yml)
