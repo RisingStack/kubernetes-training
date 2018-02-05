6044c5367fd50b2cb90c09ed25e27fb82b7601d5
Roland Szoke <fwalkwithm@gmail.com>
2018-02-05 16:22:51 +0100

    feat(istio): tracing scripts added

---
+++ istio_examples/Tracing/jaeger.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+kubectl delete -f ~/istio-0.4.0/install/kubernetes/addons/zipkin.yaml
+kubectl apply -n istio-system -f https://raw.githubusercontent.com/jaegertracing/jaeger-kubernetes/master/all-in-one/jaeger-all-in-one-template.yml
+
+POD_STATUS=""
+while [ "$POD_STATUS" != "Running" ]; do
+  sleep 10
+  POD_STATUS=$(kubectl get pods -l app=jaeger -n istio-system -o jsonpath='{.items[0].status.phase}')
+done
+
+kubectl port-forward -n istio-system $(kubectl get pod -n istio-system -l app=jaeger -o jsonpath='{.items[0].metadata.name}') 16686:16686 &
+
+echo "Jaeger running on: http://localhost:16686/"
+
+### DELETE ###
+# lsof -c kubectl -ti:16686 -a | xargs kill
+# kubectl delete -f https://raw.githubusercontent.com/jaegertracing/jaeger-kubernetes/master/all-in-one/jaeger-all-in-one-template.yml -n istio-system
---
+++ istio_examples/Tracing/zipkin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+kubectl apply -f ~/istio-0.4.0/install/kubernetes/addons/zipkin.yaml
+
+POD_STATUS=""
+while [ "$POD_STATUS" != "Running" ]; do
+  sleep 10
+  POD_STATUS=$(kubectl get pods -l app=zipkin -n istio-system -o jsonpath='{.items[0].status.phase}')
+done
+
+kubectl port-forward -n istio-system $(kubectl get pod -n istio-system -l app=zipkin -o jsonpath='{.items[0].metadata.name}') 9411:9411 &
+
+echo "Zipkin running on: http://localhost:9411/"
+
+### DELETE ###
+# lsof -c kubectl -ti:9411 -a | xargs kill
+# kubectl delete -f ~/istio-0.4.0/install/kubernetes/addons/zipkin.yaml
