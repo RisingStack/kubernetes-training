c65b5d195ad8dec925106e3dbc978fd832894c7e
Roland Szoke <fwalkwithm@gmail.com>
2018-02-06 15:47:38 +0100

    feat(istio): circuitbreaker example

--- istio_examples/CircuitBreaker/deploy.sh
+++ istio_examples/CircuitBreaker/deploy.sh
@@ -1,3 +1,10 @@
-FOLDER=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+#!/bin/bash
 
-istioctl create -f $FOLDER/circuitBreaker.yml
+DIR=$(pwd)
+
+kubectl delete destinationpolicy github-circuit-breaker
+kubectl apply -f <(istioctl kube-inject -f $DIR/circuitBreaker.yml)
+
+echo "Testing... (sending 3 request simultaneously)"
+POD=$(kubectl get pod -l app=api -o jsonpath='{.items[0].metadata.name}')
+kubectl exec -it $POD -c api  -- sh -c "curl -s -I http://github.default.svc.cluster.local:5000/api/hello/ | grep HTTP/1.1 & curl -s -I http://github.default.svc.cluster.local:5000/api/hello | grep HTTP/1.1 & curl -s -I http://github.default.svc.cluster.local:5000/api/hello | grep HTTP/1.1 & sleep 2s"
--- istio_examples/Services/destroy.sh
+++ istio_examples/Services/destroy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 kubectl delete deployment,svc,ingress,routerule,destinationpolicy --all
+
+kubectl delete -f <(istioctl kube-inject -f RateLimiting/adapter.yml)
+kubectl delete -f <(istioctl kube-inject -f RateLimiting/instance.yml)
+kubectl delete -f <(istioctl kube-inject -f RateLimiting/rule.yml)
+
+kubectl delete -f ~/istio-0.4.0/install/kubernetes/addons/prometheus.yaml
+kubectl delete -f ~/istio-0.4.0/install/kubernetes/addons/zipkin.yaml
+kubectl delete -f https://raw.githubusercontent.com/jaegertracing/jaeger-kubernetes/master/all-in-one/jaeger-all-in-one-template.yml -n istio-system
